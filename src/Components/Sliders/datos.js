b33f4d538eba7eca4e7596c4cbc4262102dc65c1
Gerson107 <danimejia1995@hotmail.com>
2022-02-20 22:07:42 -0500

    agregando imagenes

--- src/Components/Sliders/datos.js
+++ src/Components/Sliders/datos.js
@@ -29,7 +29,55 @@ const datos =[
         "ciudad": "Beijing"
         
       },
-      
+      {
+        "id": 5,
+        "image":"paris1.png",
+        "name":"France",
+        "ciudad": "Paris"
+        
+      },
+      {
+        "id": 6,
+        "image":"cairo2.png",
+        "name":"Egypt",
+        "ciudad": "Cairo"
+      },
+      {
+        "id": 7,
+        "image":"oslo1.png",
+        "name":"Norway",
+        "ciudad": "Oslo"
+      },
+      {
+        "id": 8,
+        "image":"shenzhen1.png",
+        "name":"China",
+        "ciudad": "Shenzhen"
+      },
+      {
+        "id": 9,
+        "image":"dubai1.png",
+        "name":"Arab E.",
+        "ciudad": "Dubai"
+      },
+      {
+        "id": 10,
+        "image":"sidney1.png",
+        "name":"Australia",
+        "ciudad": "Sydney"
+      },
+      {
+        "id": 11,
+        "image":"cartagena1.png",
+        "name":"Colombia",
+        "ciudad": "Cartagena"
+      },
+      {
+        "id": 12,
+        "image":"orlando2.jpg",
+        "name":"EE. UU",
+        "ciudad": "Orlando"
+      },
      
     ]
   
