c0e017d894e7924b7e139281e23995afc4b7c006
Gerson107 <danimejia1995@hotmail.com>
2022-02-25 12:33:39 -0500

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
+const cors = require('cors')
 const express = require('express');
 require('./config/database');
+const ROUTER = require('./routes/routes')
 const PORT = 4000;
 
 const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/v1', ROUTER)
 
 app.listen(PORT, ()=>console.log('Server Ready'))
