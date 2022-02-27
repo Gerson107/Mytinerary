9c1b2bb971cec20017a28c5866fb2854af871d40
Gerson107 <danimejia1995@hotmail.com>
2022-02-26 23:47:54 -0500

    modifid

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
-const cors = require('cors')
-const express = require('express');
-require('./config/database');
-const ROUTER = require('./routes/routes')
-const PORT = 4000;
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+require("./config/database");
 
+
+const ROUTER = require("./routes/routes");
+const PORT = 4000;
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/v1', ROUTER)
-
-app.listen(PORT, ()=>console.log('Server Ready'))
+//midelwares
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", ROUTER);
+app.listen(PORT, () => console.log("Server Ready"));
--- src/Pages/Cities.js
+++ src/Pages/Cities.js
@@ -1,18 +1,18 @@
 import React, {useEffect}  from 'react';
 import './Cities.css';
-import Card from '../Components/Card/Card'
+import Cards from '../Components/Card/Card'
 import axios from 'axios'
 
 function Cities(){
 
   useEffect(()=>{
     axios.get(`http://localhost:4000/api/v1/allcities`)
-    .then(respuesta=>console.log(respuesta))
+    .then(respuesta=>console.log(respuesta.data.response.cities))
   }, [])
   
     return (
       <>
-      <Card/>
+      <Cards/>
     
       </>
     )
