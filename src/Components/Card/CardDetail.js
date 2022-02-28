a630f1eee435b16cf5663242948f9f717bca6988
Gerson107 <danimejia1995@hotmail.com>
2022-02-28 10:18:04 -0500

    create components card detail

---
+++ src/Components/Card/CardDetail.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import './Card.css';
+import { Link as LinkRouter } from "react-router-dom";
+function CardDetail() {
+    const [data, setData] = useState();
+    let {id} = useParams()
+    useEffect(()=>{
+        axios
+        .get(`http://localhost:4000/api/v1/allcities`)
+        .then((respuesta) => setData(respuesta.data.response.cities.filter(city=> city._id === id)) 
+       
+        );
+       
+      }, [])
+  return (
+    <>
+ 
+    {data?.map(city =>
+        <div className='detail'>
+              <h1 className='detalletitulo'>Bienvenido a la ciudad de:  {city.ciudad}</h1>
+              <p>---our new website---</p>
+             <h2> UNDER CONSTRUCTION</h2>
+             <LinkRouter to="/cities" className="link">
+             Return Cities
+            </LinkRouter>
+             
+        </div>
+     
+
+  
+
+       
+        )}
+      
+    </>
+ )
+ }
+
+export default CardDetail
