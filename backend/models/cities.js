fb54a511a3898ffd42e38b216ab5430e4c25e118
Gerson107 <danimejia1995@hotmail.com>
2022-03-03 18:54:31 -0500

    agg dates model

--- backend/models/cities.js
+++ backend/models/cities.js
@@ -4,7 +4,13 @@ const citiesSchema = new mongoose.Schema({
     image:{type:String, required:true},
     name:{type:String, required:true},
     ciudad:{type:String, required:true},
-    description:{type:String, required:true}
+    description:{type:String, required:true},
+    flag:{type:String, required:true},
+    language:{type:String, required:true},
+    hotels:{type:String, required:true},
+    restaurants:{type:String, required:true},
+    hospitals:{type:String, required:true}
+
 })
 
 const Cities = mongoose.model('cities', citiesSchema)
