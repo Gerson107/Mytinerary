893e05f393922e474e5905afa8151a2891c0e1e4
Gerson107 <danimejia1995@hotmail.com>
2022-03-14 09:10:39 -0500

    create models users

---
+++ backend/models/users.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullName:{type:String, required:true},
+    lastName:{type:String, required:true},
+    email:{type:String, required:true},
+    password:[{type:String, required:true}],
+    profile:{type:String, required:true},
+    emailVerified:{type:Boolean, requred:true},
+    country:{type:String, required:true},
+    from:{type:Array}
+
+})
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User
