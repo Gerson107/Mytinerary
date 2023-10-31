0073e284d1aaf0c7a42705ae6754e6d3b32cc6ac
Gerson107 <danimejia1995@hotmail.com>
2023-10-30 23:28:47 -0500

    asi

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/v1", ROUTER);
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+"/client/build/index.html"))
+        res.sendFile(path.join(__dirname+"/frontend/build/index.html"))
     })
 }
 app.listen(PORT, HOST, () => console.log("Server Ready on PORT" + HOST, PORT));
